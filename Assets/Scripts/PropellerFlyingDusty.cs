2231c0f50ce7e258ea834d741ee27601308397e1
dr6401 <dr6401@student.uni-lj.si>
2025-09-25 21:51:59 +0200

    Adjusted propeller flying dusty stuff
    In the future BACKLOG the orientation point of the legs is in the middle of them and not in the heel, resulting in wierd spinning

--- Assets/Scripts/PropellerFlyingDusty.cs
+++ Assets/Scripts/PropellerFlyingDusty.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class PropellerFlyingDusty : MonoBehaviour
 {
-    [SerializeField] private float propellerSpeed = 20000f; 
+    [SerializeField] private float propellerSpeed = 2500f; 
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * (propellerSpeed * Time.deltaTime));
+        transform.Rotate(Vector3.up * (propellerSpeed * Time.deltaTime));
     }
 }
