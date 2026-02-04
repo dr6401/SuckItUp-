8f2e8c72e3151320c1179f65fa18ca450f9ddde2
dr6401 <dr6401@student.uni-lj.si>
2026-02-04 15:53:54 +0100

    made pausing easInAndOutDuration way shorter

--- Assets/SCRIPTS/GameConstants.cs
+++ Assets/SCRIPTS/GameConstants.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class GameConstants
 {
-    public const float fadeInOrOutDuration = 1.5f;
-    public const float mediumFadeInOrOutDuration = 1f;
-    public const float shortFadeInOrOutDuration = 0.5f;
+    public const float fadeInOrOutDuration = 0.5f;
+    public const float mediumFadeInOrOutDuration = 0.25f;
+    public const float shortFadeInOrOutDuration = 0.25f;
     
     
     public const int defaultTextures = 2;
