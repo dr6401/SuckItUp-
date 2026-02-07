1615e499f6f77d7cfc4ac1b837d4b9aab9680688
dr6401 <dr6401@student.uni-lj.si>
2026-02-08 00:01:55 +0100

    Changed something. idk. lens distortion broke again. pray to c# gods it wont break no more

--- Assets/SCRIPTS/GameScripts/CameraFOVController.cs
+++ Assets/SCRIPTS/GameScripts/CameraFOVController.cs
@@ -87,7 +87,7 @@ public class CameraFOVController : MonoBehaviour
         //timeSinceStoppedLensDistortionFeedback = 0;
         float currentLensDistortion =
             vacuumStartlensDistortionFeedback.Intensity.Evaluate(normalizedTime) * vacuumStartlensDistortionFeedback.RemapIntensityOne;
-        Debug.Log($"timeSinceStartedLensDistortionFeedback: {timeSinceStartedLensDistortionFeedback}, normalizedTime: {normalizedTime}, evaluated starting lens distortion: {currentLensDistortion}");
+        Debug.Log($"timeSinceStartedLensDistortionedback: {timeSinceStartedLensDistortionFeedback}, normalizedTime: {normalizedTime}, evaluated starting lens distortion: {currentLensDistortion}");
         if (vacuumStoplensDistortionFeedback != null) vacuumStoplensDistortionFeedback.RemapIntensityOne = currentLensDistortion;
         onStopVacuumCameraFXFeedback?.PlayFeedbacks();
         //StartCoroutine(PlayOnStopVacuumCameraFXFeedbackAfterAWhile());
