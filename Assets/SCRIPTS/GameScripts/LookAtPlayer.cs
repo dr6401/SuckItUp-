15bac164a5324dc01fd1a395254bbf1bb23d2d39
dr6401 <dr6401@student.uni-lj.si>
2026-02-21 20:11:42 +0100

    Added augment spinny thing to roomba + made it look at player

--- Assets/SCRIPTS/GameScripts/LookAtPlayer.cs
+++ Assets/SCRIPTS/GameScripts/LookAtPlayer.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     private Transform playerTransform;
+    [SerializeField] private bool setXRotationToZero = false;
+
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -12,9 +14,11 @@ public class LookAtPlayer : MonoBehaviour
     void Update()
     {
         Vector3 dir = playerTransform.position - transform.position;
+        
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 EulerAngles = lookRotation.eulerAngles;
-        EulerAngles.x = -20f;
+        if (setXRotationToZero) EulerAngles.x = 0f;
+        else EulerAngles.x = -20f;
         EulerAngles.z = 0f;
         transform.rotation = Quaternion.Euler(EulerAngles);
     }
